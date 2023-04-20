fb44dad0afd33947cf322891fb21f922178e4699
Ryukiju <86810366+Ryukiju@users.noreply.github.com>
2023-04-20 10:52:57 +0900

    [unrated] Title: 과일 장수, Time: 0.34 ms, Memory: 72.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int m, int[] score) {
+        int answer = 0;
+
+        List<Integer> scoreList = new ArrayList<>();
+        for(Integer num:score) {
+        	scoreList.add(num);
+        }
+        scoreList.sort(null);
+        for(int i=1; i<=scoreList.size()/m;i++) {
+        	answer+=scoreList.get(scoreList.size()-m*i)*m;
+        }
+        System.out.println(answer);
+        return answer;
+    }
+}
