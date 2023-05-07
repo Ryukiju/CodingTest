786e9ff36900153a411ce38d8840341d49d30b1f
Ryukiju <86810366+Ryukiju@users.noreply.github.com>
2023-05-07 13:28:18 +0900

    [level 2] Title: 의상, Time: 0.06 ms, Memory: 76 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42578. 의상/의상.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.util.Map.Entry;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        
+        Map<String, Integer> map = new HashMap<>();
+        
+        for(int i=0; i<clothes.length; i++) {
+        	map.put(clothes[i][1], map.getOrDefault(clothes[i][1], 0)+1);
+        }
+        
+        // System.out.println(map);
+        
+        for(Entry<String, Integer> entrySet : map.entrySet()) {
+        	answer*= entrySet.getValue()+1;
+        }
+        
+        return answer-1;
+    }
+}
