fa800412fdae8a0a4a1732fbed7950646da04eda
Ryukiju <86810366+Ryukiju@users.noreply.github.com>
2023-06-07 20:08:56 +0900

    [level 1] Title: 체육복, Time: 0.33 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.java
+++ 프로그래머스/lv1/42862. 체육복/체육복.java
@@ -3,7 +3,6 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = n-lost.length;
-        List<Integer> lostAndReserve = new ArrayList<>();
         
         Arrays.sort(lost);
         Arrays.sort(reserve);
@@ -23,17 +22,6 @@ class Solution {
         int reserveIndex = 0;
         
         while(lostIndex<lost.length&&reserveIndex<reserve.length) {
-//        	System.out.println("lostIndex :"+lostIndex+", reserveIndex :"+reserveIndex);
-//        	if(lostAndReserve.contains(lost[lostIndex])) {
-//        		answer++;
-//        		lostIndex++;
-//        		continue;
-//        	}
-//        	if(lostAndReserve.contains(reserve[reserveIndex])) {
-//        		reserveIndex++;
-//        		continue;
-//        	}
-
         	if(lost[lostIndex]-1<=reserve[reserveIndex]&&lost[lostIndex]+1>=reserve[reserveIndex]) {
         		reserveIndex++;
         		answer++;
