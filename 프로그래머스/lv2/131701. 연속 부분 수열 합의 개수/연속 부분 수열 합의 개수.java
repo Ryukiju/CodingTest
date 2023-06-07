0a29618238daa45fdac9200f803bf219033a80d0
Ryukiju <86810366+Ryukiju@users.noreply.github.com>
2023-06-07 20:23:32 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1706.64 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/lv2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,6 +1,21 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[] elements) {
-        int answer = 0;
-        return answer;
+        Set<Integer> answerSet = new HashSet<>();
+
+        //시작 인덱스
+        for(int i=0; i<=elements.length; i++) {
+        	//수열 갯수
+        	for(int j=1; j<=elements.length; j++) {
+            	int summary = 0;
+            	//수열 갯수만큼 summary
+            	for(int x=0; x<j; x++) {
+            		summary+=elements[(i+x)%elements.length];
+            	}
+            	answerSet.add(summary);
+        	}
+        }
+        return answerSet.size();
     }
 }
