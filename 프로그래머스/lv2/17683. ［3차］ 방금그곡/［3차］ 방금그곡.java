022277915c09e79ebcc6733473e29c91f45d189e
Ryukiju <86810366+Ryukiju@users.noreply.github.com>
2023-07-10 10:34:27 +0900

    [level 2] Title: [3차] 방금그곡, Time: 42.33 ms, Memory: 88.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17683. ［3차］ 방금그곡/［3차］ 방금그곡.java
@@ -0,0 +1,94 @@
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+class Solution {
+    public String solution(String m, String[] musicinfos) throws Exception{
+        String answer = null;
+        Queue<Music> answerQ = new LinkedList<>();
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+        List<String> mList = new ArrayList<>();
+        
+        //m->mList 변환
+        for(int i=m.length()-1; i>=0;i--) {
+        	if(m.charAt(i)=='#') {
+        		mList.add(m.substring(i-1, i+1));
+        		i--;
+        	}else {
+        		mList.add(m.substring(i,i+1));
+        	}
+        }
+        Collections.reverse(mList);
+        for(String musicinfo:musicinfos) {
+        	String[] musicinfoSplit =musicinfo.split(","); 
+        	long term = (sdf.parse(musicinfoSplit[1]).getTime()-sdf.parse(musicinfoSplit[0]).getTime())/1000/60;
+        	String musicName = musicinfoSplit[2];
+        	String melody = musicinfoSplit[3];
+        	List<String> melodyList = new ArrayList<>();
+        	List<String> acboList = new ArrayList<>();
+        	
+        	//melody-> melodyList 변환
+            for(int i=melody.length()-1; i>=0;i--) {
+            	if(melody.charAt(i)=='#') {
+            		melodyList.add(melody.substring(i-1, i+1));
+            		i--;
+            	}else {
+            		melodyList.add(melody.substring(i,i+1));
+            	}
+            }
+            Collections.reverse(melodyList);
+        	
+            //melodyList를 이용해 acboList 생성
+        	for(int i=0; i<term;i++) {
+        		acboList.add(melodyList.get(i%melodyList.size()));
+        	}
+//        	System.out.println("mList :"+mList);
+//        	System.out.println("melodyList :"+melodyList);
+//        	System.out.println("acboList :"+acboList);
+//        	System.out.println("======================");
+        	
+        	//acboList에서 mList에 포함하는지 체크
+        	for(int i=0; i<=acboList.size()-mList.size();i++) {
+        		boolean equal = true;
+        		for(int j=0; j<mList.size();j++) {
+        			if(!acboList.get(i+j).equals(mList.get(j))) {
+        				equal = false;
+        				break;
+        			}
+        		}
+        		if(equal) {
+        			answerQ.add(new Music(term, musicName));
+        		}
+        	}
+        	
+//        	System.out.println("=================");
+        }
+        
+        Music q = null;
+        if(!answerQ.isEmpty()) {
+        	q=answerQ.poll();
+        	answer = q.musicName;
+        }else {
+        	return "(None)";
+        }
+
+        while(!answerQ.isEmpty()) {
+        	Music ele = answerQ.poll();
+        	if(ele.term>q.term) {
+        		q = ele;
+        		answer = ele.musicName; 
+        	}
+        }
+        
+        return answer;
+    }
+}
+
+class Music{
+	long term;
+	String musicName;
+	
+	public Music(long term, String musicName) {
+		this.term = term;
+		this.musicName = musicName;
+	}
+}
